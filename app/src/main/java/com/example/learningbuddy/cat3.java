66876881a1be3a846eb21694b17c681ea0519045
shamsenurshanto <180104128@aust.edu>
2020-08-16 13:33:10 +0600

    Quiz UI and processing system added

---
+++ app/src/main/java/com/example/learningbuddy/cat3.java
@@ -0,0 +1,14 @@
+package com.example.learningbuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cat3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cat3);
+    }
+}
